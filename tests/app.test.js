0c0911730b4b7e5c6a86470d406c45b2860527cb
chandu7313 <mrcm7313@gmail.com>
2026-05-19 23:07:12 +0530

    Fix: Configure jest and fix test assertions

--- tests/app.test.js
+++ tests/app.test.js
@@ -7,7 +7,7 @@ describe('test the api', () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
         expect(res.text.replace(/\s+/g, " ").trim()).toBe(
-            "<h1>Welcome to the app</h1> <h2>Name: Kavya</h2>"
+            "<h1>Welcome to the app</h1> <h2>Name: KAVYA</h2>"
         );
     });
 
